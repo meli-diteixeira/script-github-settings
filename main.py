cf0d66fcb325ba744b9aa31875568164a0184e34
Dihogo Teixeira <dihogo.teixeira@mercadolivre.com>
2021-03-29 12:28:46 -0300

    add python resources

---
+++ main.py
@@ -0,0 +1,36 @@
+import json
+import requests
+
+def getVarFromFile(__locals__):
+    import imp
+    f = open(__locals__)
+    global data
+    data = imp.load_source('data', '', f)
+    f.close()
+
+getVarFromFile('$(pwd)/__locals__.py')
+repo = 'mercadolibre/{0}'.format(data.repository)
+branch = 'master'
+
+r = requests.put(
+    'https://api.github.com/repos/{0}/branches/{1}/protection'.format(repo, branch),
+    headers = {
+        'Accept': 'application/vnd.github.luke-cage-preview+json',
+        'Authorization': 'Token {0}'.format(data.api_token)
+    },
+    json = {
+        "restrictions": {
+            "users": [
+              "bertrandmartel"
+            ],
+            "teams": [
+              "my-team"
+            ]
+        },
+        "required_status_checks": None,
+        "enforce_admins": None,
+        "required_pull_request_reviews": None
+    }
+)
+print(r.status_code)
+print(r.json())
