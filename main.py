dc2ad4326149d5f64f89e32fd3a1ab9b88df5184
Dihogo Teixeira <dihogo.teixeira@mercadolivre.com>
2021-03-30 12:03:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 import json
 import requests
-
-def getVarFromFile(__locals__):
-    import imp
-    f = open(__locals__)
-    global data
-    data = imp.load_source('data', '', f)
-    f.close()
-
-getVarFromFile('$(pwd)/__locals__.py')
-repo = 'mercadolibre/{0}'.format(data.repository)
+from app.helpers.github_auth import GithubAuth
+repository = 'cciac-847463407359'
+repo = 'mercadolibre/{}'.format(repository)
 branch = 'master'
-
-r = requests.put(
-    'https://api.github.com/repos/{0}/branches/{1}/protection'.format(repo, branch),
-    headers = {
-        'Accept': 'application/vnd.github.luke-cage-preview+json',
-        'Authorization': 'Token {0}'.format(data.api_token)
-    },
-    json = {
-        "enforce_admins": None
-    }
-)
-print(r.status_code)
-print(r.json())
+GithubAuth.get_install_token()
+def disable_admins():
+    r = requests.put(
+        'https://api.github.com/repos/{}/branches/{}/protection'.format(repo, branch),
+        headers={
+            'Accept': 'application/vnd.github.luke-cage-preview+json',
+            'Authorization': 'Token {}'.format(GithubAuth.get_install_token()),
+            'repos_url': 'https://api.github.com/orgs/mercadolibre/repos'
+        },
+        json={
+            "restrictions": None,
+            "required_status_checks": {
+                "strict": True,
+                "contexts": [
+                    'continuous-integration',
+                    'workflow'
+                ]
+            },
+            "enforce_admins": False,
+            "required_pull_request_reviews": {
+                "dismissal_restrictions": {
+                "users": [],
+                "teams": []
+            },
+                "dismiss_stale_reviews": True,
+                "require_code_owner_reviews": False,
+                "required_approving_review_count": 1
+            }
+        }
+    )
+    return r.json()
